bb71a8cf89fdb3a567a50517232157856d9f7692
Jacob Chademwiri <jchademwiri@gmail.com>
2023-10-11 05:12:39 +0200

    build the navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,4 +1,29 @@
-const Navbar = () => {
-  return <nav>Navbar</nav>;
+import Link from 'next/link';
+import { AiFillBug } from 'react-icons/ai';
+
+const NavBar = () => {
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+      <Link href='/'>
+        <AiFillBug />
+      </Link>
+      <ul className='flex space-x-6'>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className='text-zinc-400 hover:text-zinc-100 transition-colors'
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
 };
-export default Navbar;
+export default NavBar;
